7911706a2921d92732e24a0e4025a3fedd80f78c
Varun Gangal <vgangal@Varuns-MacBook-Pro.local>
2024-11-10 09:53:24 -0500

    Converting utils to utils template due to github rules

--- NotebookLLamaScripted/utils.py
+++
@@ -1,8 +0,0 @@
-def load_all_creds():
-    creds = {}
-    creds["OPENAI_API_KEY"] = "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"
-    creds["OPENAI_ORG_ID"] = "org-4sIlU1giiO6E6KzlHtm3Fjzf"
-    creds["HF_TOKEN"] = "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"
-    creds["ELEVEN_API_KEY"] = "sk_c8ad69dda6d2c161800317563a65af4b656b711b8f11501a"
-
-    return creds
---
+++ NotebookLLamaScripted/utils_template.py
@@ -0,0 +1,8 @@
+def load_all_creds():
+    creds = {}
+    creds["OPENAI_API_KEY"] = "xxxx"
+    creds["OPENAI_ORG_ID"] = "yy"
+    creds["HF_TOKEN"] = "zz"
+    creds["ELEVEN_API_KEY"] = "kk"
+
+    return creds
